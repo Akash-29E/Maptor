944c5e8a8cb3954f606a824440bea54da422dee7
Akash Parmar <parmaraakash45@gmail.com>
2026-03-06 15:58:56 -0500

    feat: update project configuration and add deployment support

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const routeRouter = require('./routes/route');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://Akash-29E.github.io'] }));
 app.use(express.json());
 
 app.use('/api/route', routeRouter);
