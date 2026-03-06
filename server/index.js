18ed00dab12845436a002b8abe57e7ae27db3e40
Akash Parmar <parmaraakash45@gmail.com>
2026-03-06 16:36:15 -0500

    feat: add additional domains to CORS configuration

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const routeRouter = require('./routes/route');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: ['http://localhost:3000', 'https://Akash-29E.github.io', 'https://maptor.onrender.com'] }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://Akash-29E.github.io', 'https://maptor.onrender.com', 'http://maptor.ca', 'https://maptor.ca', 'http://www.maptor.ca', 'https://www.maptor.ca'] }));
 app.use(express.json());
 
 app.use('/api/route', routeRouter);
